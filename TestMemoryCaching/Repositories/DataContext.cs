30f680273ceacbd11742cbdd27b5333b6d69d1fb
Chris Stone <chris.stone-ext@cygest.fr>
2022-10-15 11:33:12 +0400

    Add Context DB

---
+++ TestMemoryCaching/Repositories/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestMemoryCaching.Entities;
+
+namespace TestMemoryCaching.Repositories
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+ 
+}
