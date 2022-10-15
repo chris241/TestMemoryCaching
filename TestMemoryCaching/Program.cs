30896350e088c5ce67855beed67c7b441e7600e5
Chris Stone <chris.stone-ext@cygest.fr>
2022-10-15 12:10:57 +0400

    MODIF

--- TestMemoryCaching/Program.cs
+++ TestMemoryCaching/Program.cs
@@ -1,3 +1,4 @@
+using TestMemoryCaching.Repositories;
 using TestMemoryCaching.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContextPool<DataContext>(options =>
+    {
+        string c = "server = MU-LTP-0017 ; DataBase=Cach ; user=sa ; password ='Qwerty@1234'";
+        options.UseSqlServer(c);
+    });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IProductService, CachedProductService>();
 builder.Services.AddMemoryCache();
